9e9795b97b3bd812cf27613b3fdbdaabe88b795e
mbarraldevs <mbarraldevs@outlook.com>
2025-11-22 10:52:28 +0100

    add curl gitignore

--- curl.sh
+++ curl.sh
@@ -1,9 +1,9 @@
 curl --request POST \
   --url https://api.circle.com/v1/w3s/compliance/screening/addresses \
-  --header 'Authorization: Bearer <token>' \
+  --header 'Authorization: Bearer TEST_API_KEY:57692d4c35db42adf5b0d98388ecf78a:3ef25a23f6850996b5d1d039097506bf' \
   --header 'Content-Type: application/json' \
   --data '{
-  "idempotencyKey": "a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11",
-  "address": "0x1bf9ad0cc2ad298c69a2995aa806ee832788218c",
+  "idempotencyKey": "b9303cec-7d2b-4ba9-85b1-7f8eb3d728ae",
+  "address": "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266",
   "chain": "ETH-SEPOLIA"
 }'

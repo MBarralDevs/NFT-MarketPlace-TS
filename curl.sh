1819342fb720d542465687d771b4eed742565b68
mbarraldevs <mbarraldevs@outlook.com>
2025-11-22 10:29:29 +0100

    compliance circle

--- curl.sh
+++ curl.sh
@@ -5,5 +5,5 @@ curl --request POST \
   --data '{
   "idempotencyKey": "a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11",
   "address": "0x1bf9ad0cc2ad298c69a2995aa806ee832788218c",
-  "chain": "MATIC-AMOY"
+  "chain": "ETH-SEPOLIA"
 }'
